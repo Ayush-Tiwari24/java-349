a9500a7a966c905af8c360742a21315fff935b18
VaRiAbleX1 <ranjanayush2021@gmail.com>
2026-01-30 14:21:17 +0530

    done

---
+++ j.java
@@ -0,0 +1,24 @@
+//write code for different variables of different data types
+public class j {
+    public static void main(String[] args) {
+        // Integer variable
+        int intVar = 10;
+        System.out.println("Integer Variable: " + intVar);
+
+        // Double variable
+        double doubleVar = 20.5;
+        System.out.println("Double Variable: " + doubleVar);
+
+        // Character variable
+        char charVar = 'A';
+        System.out.println("Character Variable: " + charVar);
+
+        // Boolean variable
+        boolean boolVar = true;
+        System.out.println("Boolean Variable: " + boolVar);
+
+        // String variable
+        String stringVar = "Hello, World!";
+        System.out.println("String Variable: " + stringVar);
+    }
+}
